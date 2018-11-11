51b8c1b7bcb9d64eed078861bb1f6b888cb0b76a
Kai Chang <kaijchang@gmail.com>
2018-11-11 12:42:26 -0800

    finish basic idler

--- README.md
+++ README.md
@@ -3,14 +3,16 @@ Idle Master (Python branch)
 
 This program will determine which of your Steam games still have Steam Trading Card drops remaining, and will go through each application to simulate you being “in-game” so that cards will drop.  It will check periodically to see if the game you’re idling has card drops remaining.  When only one drop remains, it will start checking more frequently.  When the game you’re idling has no more cards, it’ll move on to the next game.  When no more cards are available, the program will terminate.
 
-This is the Python repository which is helpful for user running the application on non-Windows operating systems or those having trouble getting the newest version of Idle Master for Windows to run correctly.  
+This fork focuses on compatibility with python3 and OSX.
+
+The primary goal of the fork is to provide a simpler, more readable, extensible, and customizable python wrapper for Idle Master because the original wrapper isn't maintained, so it's still on python2 and doesn't work with Steam's new cookie system.
 
 For the latest Windows version of Idle Master, [click here](https://github.com/jshackles/idle_master).
 
 Requirements
 -------
 
-This application requires Steam to be open and for you to be logged in.  This program works with Windows, OSX, and Linux.  Non-Windows version are available in the "Releases" folder.
+This application requires Steam to be open and for you to be logged in.
 
 Setup
 -------
@@ -20,13 +22,13 @@ Please read the included Setup Instructions.pdf file for detailed setup instruct
 Authors
 -------
 
-jshackles and Stumpokapow
+Optimized and rewritten for python3 and OSX by kajchang
 
-Optimized and rewritten by kajchang.
+jshackles and Stumpokapow
 
 steam-idle was writen in C# using Steamworks.NET and CSteamworks by Riley Labrecque
 
 License
 -------
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation.  A copy of the GNU General Public License can be found at http://www.gnu.org/licenses/.  For your convenience, a copy of this license is included.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation.  A copy of the GNU General Public License can be found at [http://www.gnu.org/licenses/](http://www.gnu.org/licenses/).  For your convenience, a copy of this license is included.
--- start.py
+++ start.py
@@ -3,10 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import re
 import subprocess
-import sys
 import logging
-import datetime
-import ctypes
 import json
 
 from colorama import init, Fore
@@ -19,7 +16,7 @@ class Game:
         self.gameName = gameName
 
     def __repr__(self):
-        return "{gameName} ({gameId}) - {cardsLeft}".format(**self.__dict__)
+        return "{gameName} ({gameId}) - {cardsLeft} card(s) left".format(**self.__dict__)
 
 
 class SteamIdle:
@@ -43,31 +40,89 @@ class SteamIdle:
         self.session.cookies.set(name="sessionid", value=settings["sessionid"])
         self.session.cookies.set(name="steamparental", value=settings["steamparental"])
 
-        self.id_ = settings["steamLoginSecure"][:17]
+        self.accountId = settings["steamLoginSecure"][:17]
 
-        self.logger.info("Finding games that have card drops remaining")
+        self.logger.info(Fore.GREEN + "Finding games that have card drops remaining" + Fore.RESET)
 
-        self.games = self.getGames()
+        self.gamesLeft = self.getGames()
+
+    def main(self):
+        try:
+            while self.gamesLeft:
+                game = self.gamesLeft[0]
+
+                self.startIdling(game)
+
+                while game.cardsLeft != 0:
+                    self.logger.info(Fore.BLUE + "Sleeping for {} minutes".format(5 * game.cardsLeft) + Fore.RESET)
+                    time.sleep(5 * game.cardsLeft * 60)
+
+                    self.updateCardsLeft(game)
+
+                    if game.cardsLeft == 0:
+                        self.gamesLeft.remove(game)
+                        self.stopIdling()
+
+                        self.logger.info(Fore.GREEN + "Finished idling {}, {} games left".format(game.gameName, len(self.gamesLeft)) + Fore.RESET)
+
+            self.logger.info(Fore.GREEN + "Done idling!" + Fore.RESET)
+
+        except KeyboardInterrupt:
+            self.stopIdling()
+
+    def updateCardsLeft(self, game):
+        self.logger.info(Fore.GREEN + "Checking how many card drops {} has left".format(game.gameName) + Fore.RESET)
+
+        gameSoup = BeautifulSoup(
+            self.session.get("https://steamcommunity.com/profiles/{}/gamecards/{}/".format(self.accountId, game.gameId)).text,
+            "html.parser"
+        )
+
+        game.cardsLeft = int(re.match(r"([0-9]+|(No)) card drops? remaining", gameSoup.find("span", {"class": "progress_info_bold"}).text).group(1)) if re.match(r"([0-9]+|(No)) card drops? remaining", gameSoup.find("span", {"class": "progress_info_bold"}).text).group(1) != "No" else 0
+
+        self.logger.info(Fore.GREEN + "{} has {} card drop(s) left".format(game.gameName, game.cardsLeft) + Fore.RESET)
+
+        return game.cardsLeft
 
     def getGames(self):
         gameSoup = BeautifulSoup(
-            self.session.get("http://steamcommunity.com/profiles/{}/badges".format(self.id_)).text,
-            "html.parser")
+            self.session.get("http://steamcommunity.com/profiles/{}/badges".format(self.accountId)).text,
+            "html.parser"
+        )
 
-        games = [
-            Game(
-                int(re.match(r"([0-9]+|(No)) card drops? remaining", game.find("span", {"class": "progress_info_bold"}).text).group(1)) if re.match(r"([0-9]+|(No)) card drops? remaining", game.find("span", {"class": "progress_info_bold"}).text).group(1) != "No" else 0,
-                int(re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", game.find("a", {"class": "badge_row_overlay"})["href"]).group(1)),
-                game.find("div", {"class": "badge_title"}).text.replace("View details", "").strip()
-            ) for game in filter(lambda p: re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", p.find("a", {"class": "badge_row_overlay"})["href"]) and p.find("span", {"class": "progress_info_bold"}), gameSoup.find_all("div", {"class": "badge_row"})[:-1])
-        ]
+        games = list(
+            filter(lambda game: game.cardsLeft != 0, [
+                Game(
+                    int(re.match(r"([0-9]+|(No)) card drops? remaining", game.find("span", {"class": "progress_info_bold"}).text).group(1)) if re.match(r"([0-9]+|(No)) card drops? remaining", game.find("span", {"class": "progress_info_bold"}).text).group(1) != "No" else 0,
+                    int(re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", game.find("a", {"class": "badge_row_overlay"})["href"]).group(1)),
+                    game.find("div", {"class": "badge_title"}).text.replace("View details", "").strip()
+                ) for game in filter(lambda p: re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", p.find("a", {"class": "badge_row_overlay"})["href"]) and p.find("span", {"class": "progress_info_bold"}), gameSoup.find_all("div", {"class": "badge_row"}))
+            ])
+        )
 
-        self.logger.info(Fore.GREEN + "Found {} games and {} trading cards to idle".format(
+        self.logger.info(Fore.GREEN + "Found {} games and {} trading cards to idle:".format(
             len(games),
             sum(game.cardsLeft for game in games)) + Fore.RESET)
 
+        for game in games:
+            self.logger.info(Fore.WHITE + repr(game) + Fore.RESET)
+
         return games
+
+    def startIdling(self, game):
+        self.logger.info(Fore.GREEN + "Starting to idle {}".format(game) + Fore.RESET)
+        try:
+            self.child = subprocess.Popen(["./steam-idle", str(game.gameId)])
+
+        except PermissionError:
+            subprocess.Popen(["chmod", "+x", "./steam-idle"])
+            self.child = subprocess.Popen(["./steam-idle", str(game.gameId)])
+
+    def stopIdling(self):
+        self.logger.info(Fore.GREEN + "Killing Idle Master process" + Fore.RESET)
+        self.child.terminate()
 
 
 with open("settings.json") as settings_file:
-    SteamIdle(**json.load(settings_file))
+    idler = SteamIdle(**json.load(settings_file))
+    idler.main()

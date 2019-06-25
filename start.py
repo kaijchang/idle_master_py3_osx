c65ff70fca79fa9be20b761fcffb9fc5c6abf84a
Kai Chang <kaijchang@gmail.com>
2019-06-24 20:37:02 -0700

    feat: snake_case

--- start.py
+++ start.py
@@ -11,13 +11,13 @@ from colorama import init, Fore
 
 
 class Game:
-    def __init__(self, cardsLeft, gameId, gameName):
-        self.cardsLeft = cardsLeft
-        self.gameId = gameId
-        self.gameName = gameName
+    def __init__(self, cards_left: int, game_id: str, game_name: str):
+        self.cards_left = cards_left
+        self.game_id = game_id
+        self.game_name = game_name
 
     def __repr__(self):
-        return "{gameName} ({gameId}) - {cardsLeft} card(s) left".format(**self.__dict__)
+        return "{game_name} ({game_id}) - {cards_left} card(s) left".format(**self.__dict__)
 
 
 class SteamIdle:
@@ -40,94 +40,94 @@ class SteamIdle:
         self.session.cookies = browser_cookie3.load(domain_name="steamcommunity.com")
 
         try:
-            self.accountId = requests.utils.dict_from_cookiejar(self.session.cookies)["steamLoginSecure"][:17]
+            self.account_id = requests.utils.dict_from_cookiejar(self.session.cookies)["steamLoginSecure"][:17]
         except KeyError:
             self.logger.error(Fore.RED + "Unable to load cookies, login into https://steamcommunity.com then try again." + Fore.RESET)
 
         self.sort = settings["sort"]
         self.blacklist = settings["blacklist"]
-        self.delayPerCard = settings["delayPerCard"]
+        self.delay_per_card = settings["delayPerCard"]
 
         self.logger.info(Fore.GREEN + "Finding games that have card drops remaining" + Fore.RESET)
 
-        self.gamesLeft = self.getGames()
+        self.games_left = self.get_games()
 
     def main(self):
         try:
-            while self.gamesLeft:
-                game = self.gamesLeft[0]
+            while self.games_left:
+                game = self.games_left[0]
 
-                self.startIdling(game)
+                self.start_idling(game)
 
-                while game.cardsLeft != 0:
-                    self.logger.info(Fore.BLUE + "Sleeping for {} minutes".format(self.delayPerCard * game.cardsLeft) + Fore.RESET)
-                    time.sleep(self.delayPerCard * game.cardsLeft * 60)
+                while game.cards_left != 0:
+                    self.logger.info(Fore.BLUE + "Sleeping for {} minutes".format(self.delay_per_card * game.cards_left) + Fore.RESET)
+                    time.sleep(self.delay_per_card * game.cards_left * 60)
 
-                    self.updateCardsLeft(game)
+                    self.update_cards_left(game)
 
-                    if game.cardsLeft == 0:
-                        self.gamesLeft.remove(game)
-                        self.stopIdling()
+                    if game.cards_left == 0:
+                        self.games_left.remove(game)
+                        self.stop_idling()
 
-                        self.logger.info(Fore.GREEN + "Finished idling {}, {} games left".format(game.gameName, len(self.gamesLeft)) + Fore.RESET)
+                        self.logger.info(Fore.GREEN + "Finished idling {}, {} games left".format(game.game_name, len(self.games_left)) + Fore.RESET)
 
             self.logger.info(Fore.GREEN + "Done idling!" + Fore.RESET)
 
         except KeyboardInterrupt:
-            self.stopIdling()
+            self.stop_idling()
 
-    def updateCardsLeft(self, game):
-        self.logger.info(Fore.GREEN + "Checking how many card drops {} has left".format(game.gameName) + Fore.RESET)
+    def update_cards_left(self, game: Game):
+        self.logger.info(Fore.GREEN + "Checking how many card drops {} has left".format(game.game_name) + Fore.RESET)
 
-        gameSoup = BeautifulSoup(
-            self.session.get("https://steamcommunity.com/profiles/{}/gamecards/{}/".format(self.accountId, game.gameId)).text,
+        game_soup = BeautifulSoup(
+            self.session.get("https://steamcommunity.com/profiles/{}/gamecards/{}/".format(self.account_id, game.game_id)).text,
             "html.parser"
         )
 
-        game.cardsLeft = int(re.match(r"([0-9]+|(No)) card drops? remaining", gameSoup.find("span", {"class": "progress_info_bold"}).text).group(1)) if re.match(r"([0-9]+|(No)) card drops? remaining", gameSoup.find("span", {"class": "progress_info_bold"}).text).group(1) != "No" else 0
+        game.cards_left = int(re.match(r"([0-9]+|(No)) card drops? remaining", game_soup.find("span", {"class": "progress_info_bold"}).text).group(1)) if re.match(r"([0-9]+|(No)) card drops? remaining", game_soup.find("span", {"class": "progress_info_bold"}).text).group(1) != "No" else 0
 
-        self.logger.info(Fore.GREEN + "{} has {} card drop(s) left".format(game.gameName, game.cardsLeft) + Fore.RESET)
+        self.logger.info(Fore.GREEN + "{} has {} card drop(s) left".format(game.game_name, game.cards_left) + Fore.RESET)
 
-        return game.cardsLeft
+        return game.cards_left
 
-    def getGames(self):
-        gameSoup = BeautifulSoup(
-            self.session.get("https://steamcommunity.com/profiles/{}/badges".format(self.accountId)).text,
+    def get_games(self):
+        game_soup = BeautifulSoup(
+            self.session.get("https://steamcommunity.com/profiles/{}/badges".format(self.account_id)).text,
             "html.parser"
         )
 
         games = list(
-            filter(lambda game: game.cardsLeft != 0, [
+            filter(lambda game: game.cards_left != 0, [
                 Game(
                     int(re.match(r"([0-9]+|(No)) card drops? remaining", game.find("span", {"class": "progress_info_bold"}).text).group(1)) if re.match(r"([0-9]+|(No)) card drops? remaining", game.find("span", {"class": "progress_info_bold"}).text).group(1) != "No" else 0,
                     int(re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", game.find("a", {"class": "badge_row_overlay"})["href"]).group(1)),
                     game.find("div", {"class": "badge_title"}).text.replace("View details", "").strip()
-                ) for game in filter(lambda p: re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", p.find("a", {"class": "badge_row_overlay"})["href"]) and p.find("span", {"class": "progress_info_bold"}), gameSoup.find_all("div", {"class": "badge_row"}))
+                ) for game in filter(lambda p: re.match(r"https:\/\/steamcommunity.com\/id\/.+\/gamecards\/([0-9]{6})\/", p.find("a", {"class": "badge_row_overlay"})["href"]) and p.find("span", {"class": "progress_info_bold"}), game_soup.find_all("div", {"class": "badge_row"}))
             ])
         )
 
         if self.blacklist:
             self.logger.info(Fore.BLUE + "Applying blacklist" + Fore.RESET)
 
-            gamesBeforeBlacklist = len(games)
+            games_before_blacklist = len(games)
 
             games = list(
-                filter(lambda game: game.gameId not in self.blacklist, games)
+                filter(lambda game: game.game_id not in self.blacklist, games)
             )
 
-            self.logger.info(Fore.BLUE + "The blacklist removed {} games".format(gamesBeforeBlacklist - len(games)) + Fore.RESET)
+            self.logger.info(Fore.BLUE + "The blacklist removed {} games".format(games_before_blacklist - len(games)) + Fore.RESET)
 
         else:
             self.logger.info(Fore.BLUE + "No blacklist found" + Fore.RESET)
 
         if self.sort:
             if self.sort == "leastcards":
-                games.sort(key=lambda game: game.cardsLeft)
+                games.sort(key=lambda game: game.cards_left)
 
                 self.logger.info(Fore.BLUE + "Sorted from least to most card drops remaining" + Fore.RESET)
 
             elif self.sort == "mostcards":
-                games.sort(key=lambda game: game.cardsLeft, reverse=True)
+                games.sort(key=lambda game: game.cards_left, reverse=True)
 
                 self.logger.info(Fore.BLUE + "Sorted from most to least card drops remaining" + Fore.RESET)
 
@@ -139,23 +139,23 @@ class SteamIdle:
 
         self.logger.info(Fore.GREEN + "Found {} games and {} trading cards to idle:".format(
             len(games),
-            sum(game.cardsLeft for game in games)) + Fore.RESET)
+            sum(game.cards_left for game in games)) + Fore.RESET)
 
         for game in games:
             self.logger.info(Fore.WHITE + repr(game) + Fore.RESET)
 
         return games
 
-    def startIdling(self, game):
+    def start_idling(self, game: Game):
         self.logger.info(Fore.GREEN + "Starting to idle {}".format(game) + Fore.RESET)
         try:
-            self.child = subprocess.Popen(["./steam-idle", str(game.gameId)])
+            self.child = subprocess.Popen(["./steam-idle", str(game.game_id)])
 
         except PermissionError:
             subprocess.Popen(["chmod", "+x", "./steam-idle"])
-            self.child = subprocess.Popen(["./steam-idle", str(game.gameId)])
+            self.child = subprocess.Popen(["./steam-idle", str(game.game_id)])
 
-    def stopIdling(self):
+    def stop_idling(self):
         self.logger.info(Fore.GREEN + "Killing steam-idle process" + Fore.RESET)
         self.child.terminate()
 
